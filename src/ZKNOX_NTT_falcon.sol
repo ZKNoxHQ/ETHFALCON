6b396277a3b6e72a871f8b8d45c6a64087071127
Simon Masson <simon.masson@protonmail.com>
2026-02-09 10:28:01 +0100

    remove headers

--- src/ZKNOX_NTT_falcon.sol
+++ src/ZKNOX_NTT_falcon.sol
@@ -1,48 +1,8 @@
-/**
- *
- */
-/*ZZZZZZZZZZZZZZZZZZZKKKKKKKKK    KKKKKKKNNNNNNNN        NNNNNNNN     OOOOOOOOO     XXXXXXX       XXXXXXX                         ..../&@&#.       .###%@@@#, ..
-/*Z:::::::::::::::::ZK:::::::K    K:::::KN:::::::N       N::::::N   OO:::::::::OO   X:::::X       X:::::X                      ...(@@* .... .           &#//%@@&,.
-/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::::N      N::::::N OO:::::::::::::OO X:::::X       X:::::X                    ..*@@.........              .@#%%(%&@&..
-/*Z:::ZZZZZZZZ:::::Z K:::::::K   K::::::KN:::::::::N     N::::::NO:::::::OOO:::::::OX::::::X     X::::::X                   .*@( ........ .  .&@@@@.      .@%%%%%#&@@.
-/*ZZZZZ     Z:::::Z  KK::::::K  K:::::KKKN::::::::::N    N::::::NO::::::O   O::::::OXXX:::::X   X::::::XX                ...&@ ......... .  &.     .@      /@%%%%%%&@@#
-/*        Z:::::Z      K:::::K K:::::K   N:::::::::::N   N::::::NO:::::O     O:::::O   X:::::X X:::::X                   ..@( .......... .  &.     ,&      /@%%%%&&&&@@@.
-/*       Z:::::Z       K::::::K:::::K    N:::::::N::::N  N::::::NO:::::O     O:::::O    X:::::X:::::X                   ..&% ...........     .@%(#@#      ,@%%%%&&&&&@@@%.
-/*      Z:::::Z        K:::::::::::K     N::::::N N::::N N::::::NO:::::O     O:::::O     X:::::::::X                   ..,@ ............                 *@%%%&%&&&&&&@@@.
-/*     Z:::::Z         K:::::::::::K     N::::::N  N::::N:::::::NO:::::O     O:::::O     X:::::::::X                  ..(@ .............             ,#@&&&&&&&&&&&&@@@@*
-/*    Z:::::Z          K::::::K:::::K    N::::::N   N:::::::::::NO:::::O     O:::::O    X:::::X:::::X                   .*@..............  . ..,(%&@@&&&&&&&&&&&&&&&&@@@@,
-/*   Z:::::Z           K:::::K K:::::K   N::::::N    N::::::::::NO:::::O     O:::::O   X:::::X X:::::X                 ...&#............. *@@&&&&&&&&&&&&&&&&&&&&@@&@@@@&
-/*ZZZ:::::Z     ZZZZZKK::::::K  K:::::KKKN::::::N     N:::::::::NO::::::O   O::::::OXXX:::::X   X::::::XX               ...@/.......... *@@@@. ,@@.  &@&&&&&&@@@@@@@@@@@.
-/*Z::::::ZZZZZZZZ:::ZK:::::::K   K::::::KN::::::N      N::::::::NO:::::::OOO:::::::OX::::::X     X::::::X               ....&#..........@@@, *@@&&&@% .@@@@@@@@@@@@@@@&
-/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::N       N:::::::N OO:::::::::::::OO X:::::X       X:::::X                ....*@.,......,@@@...@@@@@@&..%@@@@@@@@@@@@@/
-/*Z:::::::::::::::::ZK:::::::K    K:::::KN::::::N        N::::::N   OO:::::::::OO   X:::::X       X:::::X                   ...*@,,.....%@@@,.........%@@@@@@@@@@@@(
-/*ZZZZZZZZZZZZZZZZZZZKKKKKKKKK    KKKKKKKNNNNNNNN         NNNNNNN     OOOOOOOOO     XXXXXXX       XXXXXXX                      ...&@,....*@@@@@ ..,@@@@@@@@@@@@@&.
-/*                                                                                                                                   ....,(&@@&..,,,/@&#*. .
-/*                                                                                                                                    ......(&.,.,,/&@,.
-/*                                                                                                                                      .....,%*.,*@%
-/*                                                                                                                                    .#@@@&(&@*,,*@@%,..
-/*                                                                                                                                    .##,,,**$.,,*@@@@@%.
-/*                                                                                                                                     *(%%&&@(,,**@@@@@&
-/*                                                                                                                                      . .  .#@((@@(*,**
-/*                                                                                                                                             . (*. .
-/*                                                                                                                                              .*/
-///* Copyright (C) 2025 - Renaud Dubois, Simon Masson - This file is part of ZKNOX project
-///* License: This software is licensed under MIT License
-///* This Code may be reused including this header, license and copyright notice.
-///* See LICENSE file at the root folder of the project.
-///* FILE: ZKNOX_NTT_falcon.sol
-///* Description: Compute Negative Wrap Convolution NTT as specified in EIP-NTT, specific FALCON case
-/**
- * OPTIMIZATION vs previous version:
- *
- * Pointer-based butterfly inner loop (same technique as Dilithium NTT):
- *   - Eliminated `add(aBase, shl(5, j))` index→address conversion (6 gas/butterfly)
- *   - Inner loop walks a pointer directly: ptr += 32
- *   - nttInv final scaling loop also pointer-based
- *
- * For Falcon-512 (n=512, 9 layers, 2304 butterflies/NTT):
- *   Savings: ~6 gas × 2304 = ~14k gas per NTT call
- */
+// Copyright (C) 2026 - ZKNOX
+// License: This software is licensed under MIT License
+// This Code may be reused including this header, license and copyright notice.
+// FILE: ZKNOX_NTT_falcon.sol
+// Description: verify falcon core component
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
